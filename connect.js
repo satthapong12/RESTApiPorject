9f98452403e82cfb681fa3940214806853d799ad
satthapong12 <satthapongpunpa12@gmail.com>
2024-09-20 14:39:09 +0700

    Update code Connect

--- connect.js
+++ connect.js
@@ -1,14 +1,33 @@
-// connect.js
-const mysql = require('mysql2/promise'); // ใช้ mysql2/promise
+const mysql = require('mysql2/promise');
+const os = require('os');
+
+// ฟังก์ชันดึง IP address
+function getLocalIp() {
+    const interfaces = os.networkInterfaces();
+    for (const interfaceName in interfaces) {
+        const addresses = interfaces[interfaceName];
+        for (const i in addresses) {
+            const address = addresses[i];
+            if (address.family === 'IPv4' && !address.internal) {
+                return address.address;
+            }
+        }
+    }
+    return '127.0.0.1'; // กรณีหาไม่เจอ ให้ใช้ localhost
+}
+
+const host = getLocalIp(); // ดึง IP address ของเครื่อง
 
 // ตั้งค่าการเชื่อมต่อฐานข้อมูล
 const db = mysql.createPool({
-    host: '192.168.1.104',
+    host: host, // ใช้ IP address ที่ดึงมาได้
     user: 'root',
     password: 'Cs210245',
     database: 'pro1_register'
 });
+
 db.getConnection()
-    .then(() => console.log('Connect to Database'))
+    .then(() => console.log('Connected to Database at', host))
     .catch(err => console.error('Error connecting to database:', err.stack));
+
 module.exports = db;
